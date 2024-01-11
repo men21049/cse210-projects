8953bc492fe79208d4c0c01e0704d0aa12fc50df
men21049 <104547119+men21049@users.noreply.github.com>
2024-01-10 21:39:25 -0600

    Fix error in program 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool found = true;
+        while (found)
+        {
+            string matching;
+            Console.WriteLine("What is the magic number?  18");
+            Console.WriteLine("What is your guess?");
+            int answer = int.Parse(Console.ReadLine());
+            if (answer < 18)
+            {
+                matching = "Higher";
+            }
+            else if (answer > 18)
+            {
+                matching = "Lower";
+            }
+            else
+            {
+                matching = "You guessed it!";
+                found = false;
+            }
+            Console.WriteLine($"{matching}");
+
+        }
     }
 }
