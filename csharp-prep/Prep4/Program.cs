a2d636ab2279bb668b743a25972d19d4827e1c6d
men21049 <104547119+men21049@users.noreply.github.com>
2024-01-10 21:40:17 -0600

    Fix error in program 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int answer = 1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (answer != 0)
+        {
+            Console.WriteLine("Enter number:");
+            answer = int.Parse(Console.ReadLine());
+            numbers.Add(answer);
+        }
+        int sum = numbers.Sum();
+        double average = numbers.Average();
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
