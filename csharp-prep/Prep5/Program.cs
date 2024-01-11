805608caef1f146f3c9258de7260d5af1ec3d23e
men21049 <men21049@byui.edu>
2024-01-10 21:02:30 -0600

    fourth program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = SquareNumber(PromptUserNumber());
+        DisplayResult(name, number);
+    }
+
+    public static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    public static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    public static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    public static void DisplayResult(string name, int num)
+    {
+        Console.WriteLine($"{name}, the square of your number is {num}");
     }
 }
